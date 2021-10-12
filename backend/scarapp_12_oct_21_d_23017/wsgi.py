4c043186a4b2c60debaaec69ea267bf62d6db8d0
Crowdbotics <engineering@crowdbotics.com>
2021-10-12 10:08:45 +0000

    Update models

--- backend/scarapp_12_oct_21_d_23017/wsgi.py
+++ backend/scarapp_12_oct_21_d_23017/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scarapp_12_oct_21_d_23017.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scarapp_12_oct_21_d_23017.settings")
 
 application = get_wsgi_application()
